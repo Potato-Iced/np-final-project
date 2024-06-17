52af6a55dfeaf5e994ef49881197567b904d9c8a
Potato-Iced <snack8295@gmail.com>
2024-06-17 23:18:28 +0900

    메세지 포맷 구현 성공; 서버에서 키 입력 받기 성공;

--- Client/TCPclient.c
+++ Client/TCPclient.c
@@ -4,13 +4,16 @@
 #include <time.h>
 
 #define	BUF_SIZE	512
+#define A_LEN		2
+#define R_LEN		2
+#define M_LEN		3
 
 void ErrorDisplay( char *str )
 {
 	printf("<ERROR> %s!!!\n", str );
 	exit(-1);
 }
- 
+
 // 사용자 정의 데이터 수신 함수
 int recvn( SOCKET s, char *buf, int len, int flags )
 {
@@ -30,10 +33,12 @@ int recvn( SOCKET s, char *buf, int len, int flags )
 	return (len - left);
 }
 
+
+
 int main( int argc, char* argv[] )
 {
 	int		retval, x, y;
-
+	int		rcvLen, targetLen;
 	WSADATA	wsa;
 	retval = WSAStartup(  MAKEWORD(2, 2), &wsa );
 	if(retval != 0)	return -1;
@@ -50,10 +55,10 @@ int main( int argc, char* argv[] )
 	ServerAddr.sin_port			= htons(9000);
 	ServerAddr.sin_addr.s_addr	= inet_addr("127.0.0.1");
 	
-	// 난수 생성
+	// 0 ~ 200 난수 생성
 	srand(time(NULL));
-	x = rand();
-	y = rand();
+	x = rand() % 201;
+	y = rand() % 201;
 
 
 	retval = connect(ClientSocket, (SOCKADDR *)&ServerAddr, sizeof(ServerAddr));
@@ -61,13 +66,50 @@ int main( int argc, char* argv[] )
 		ErrorDisplay("connect() error(SOCKET_ERROR)");
 	}
 
-	char		Buf[BUF_SIZE+1];
+	char		Buf[BUF_SIZE+1], itemp[16] = {0,};
+	//char*		cPtr;
 	//int		iLen;
-	while(1)
+	ZeroMemory(Buf, sizeof(Buf));
+
+	
+	printf("[TCP 클라이언트] 최초 위치 초기화.. 현재 좌표 전송 (%d, %d)\n", x, y);
+	// 명령어 생성 - add
+	// 첫 글자에 개수
+	Buf[0] = (char)2;
+	// 나머지는 x, y 대입
+	*(int*)(Buf + 1) = x;
+	*(int*)(Buf + (1 + sizeof(int))) = y;
+	// 마지막에 명령어 종류, 최초 초기화이므로 a
+	*(int*)(Buf + 1 + A_LEN * sizeof(int)) = 'a';
+
+	retval = send(ClientSocket, Buf, 2 * sizeof(int) + 3, 0);
+	if (retval == SOCKET_ERROR) {
+		printf("<ERROR> send(%s) falied.\n", Buf);
+	}
+
+	printf("[TCP 클라이언트] 성공적으로 메세지를 보냈습니다. (%dB)\n", retval);
+	printf("cnt : %d\n", (int)Buf[0]);
+	for (int i = 0; i < A_LEN; i++) {
+		printf("좌표%d : %d\n", i + 1, *(int*)(Buf + (1 + i * sizeof(int))));
+	}
+	printf("command : %c\n", Buf[2 * sizeof(int) + 1]);
+
+
+
+
+	
+	while (1)
 	{
 		ZeroMemory(Buf, sizeof(Buf));
-		printf("\n[보낼 데이타] ");
-		if(fgets( Buf, BUF_SIZE+1, stdin ) == NULL)	break;
+		// 명령어 생성 - refresh
+		// 첫 글자에 개수
+		Buf[0] = (char)2;
+		// 나머지는 x, y 대입
+		*(int*)(Buf + 1) = x;
+		*(int*)(Buf + (1 + sizeof(int))) = y;
+		// 마지막에 명령어 종류, 최초 초기화이므로 a
+		*(int*)(Buf + 1 + A_LEN * sizeof(int)) = 'r';
+		
 		retval = send(ClientSocket, Buf, strlen(Buf), 0);
 		if(retval == SOCKET_ERROR) {
 			printf("<ERROR> send()(SOCKET_ERROR)!!!\n");
@@ -75,6 +117,8 @@ int main( int argc, char* argv[] )
 		}
 		printf("[TCP 클라이언트] %d 바이트를 보냈습니다.\n", retval);
 
+		// 원래 서버는 에코로 다시 전송, 기존에 보낸 바이트 수와 받아야 하는 바이트 수가 같음
+		// 근데 이 바이트 수가 다르면 서버에서 정렬 
 		retval = recvn( ClientSocket, Buf, retval, 0 );
 		if(retval == SOCKET_ERROR) {
 			printf("<ERROR> recvn()(SOCKET_ERROR)!!!\n");
@@ -84,7 +128,9 @@ int main( int argc, char* argv[] )
 		Buf[retval]= '\0';
 		printf("[TCP 클라이언트] %d 바이트를 받았습니다.\n", retval);
 		printf("[받은 데이타] %s \n", Buf);
+		Sleep(1000);
 	}
+	
 	closesocket( ClientSocket );
 	WSACleanup();
 	return 0;
--- Server/echo_selectserv_win - lab.c
+++ Server/echo_selectserv_win - lab.c
@@ -4,16 +4,26 @@
 #include <winsock2.h>
 #include <Windows.h>
 #define BUF_SIZE 1024
+#define DRONE_AMOUNT 3	// 드론 개수
+
+typedef struct pos {	// 드론 정보를 담을 구조체
+	int port;	// 식별용 포트
+	int x;		// x 좌표
+	int y;		// y 좌표
+}POS;
 void ErrorHandling(char *message);
+POS* RoutePlanner(POS*);	// 드론 이동 경로 
 
 int main(void)
 {
+	POS				droneList[DRONE_AMOUNT] = { 0, };
 	WSADATA			wsaData;
 	SOCKET			hServSock, hClntSock;
 	SOCKADDR_IN		servAdr, clntAdr, clientaddr;
 	int				adrSz;
 	int				strLen, fdNum, i;
 	char			buf[BUF_SIZE];
+	char*			split;	// buf를 공백 기준으로 파싱해서 구분, strtok() 사용
 	int				addrlen;
 
 	if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
@@ -35,64 +45,85 @@ int main(void)
 	FD_ZERO(&reads);
 	FD_SET(hServSock, &reads);
 
-	while(1)
-	{	
+	while (1)
+	{
+		printf("M 키를 눌러 드론 이동 모드, Q키를 눌러 드론 정렬 및 종료\n");
 		// select() 함수 호출
 		cpyReads = reads;
 		timeout.tv_sec = 5;
 		timeout.tv_usec = 5000;
 
-		fdNum = select(0, &cpyReads, 0, 0, &timeout );
+		fdNum = select(0, &cpyReads, 0, 0, &timeout);
 		if (fdNum == SOCKET_ERROR) {
 			closesocket(hClntSock);
-			break; 
+			break;
 		}
-		else if (fdNum == 0) {
-			continue;
-		}
+		// else if(fdNum == 0) continue; // 이 부분 삭제, 아래로 변경
+		else if (fdNum != 0){
+			// signal이 온 상태에서 signal 온 핸들 확인 작업 수행 LOOP 생성.
+			for (i = 0; i < reads.fd_count; i++) {
+				// signal 온 핸들 확인.
+				if (FD_ISSET(reads.fd_array[i], &cpyReads)) {
+					if (reads.fd_array[i] == hServSock) {
+						// accept 실행...
+						// client의 연결 요청 수신.
+						adrSz = sizeof(clntAdr);
+						printf("server> wait a client connection.\n");
+						hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &adrSz);
+						printf("server> connected client: Port:%d, IP:%s \n",
+							clntAdr.sin_port, inet_ntoa(clntAdr.sin_addr));
 
-		// signal이 온 상태에서 signal 온 핸들 확인 작업 수행 LOOP 생성.
-		for (i = 0; i<reads.fd_count; i++) {
-
-			// signal 온 핸들 확인.
-			if (FD_ISSET(reads.fd_array[i], &cpyReads)) {
-
-				if (reads.fd_array[i] == hServSock) {
-					// accept 실행...
-					// client의 연결 요청 수신.
-					adrSz = sizeof(clntAdr);
-					printf("server> wait a client connection.\n");
-					hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &adrSz);
-					printf("server> connected client: Port:%d, IP:%s \n",
-						clntAdr.sin_port, inet_ntoa(clntAdr.sin_addr));
-
-					FD_SET(hClntSock, &reads);
-				}
-				else {
-					// data 수신.
-					strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
-					if (strLen <= 0)    // close request!
-					{
-						closesocket(reads.fd_array[i]);
-						FD_CLR(reads.fd_array[i], &reads);
-						printf("server> close connection with (port:%d, IP:%s)\n",
-							clientaddr.sin_port, inet_ntoa(clientaddr.sin_addr));
-						break;
+						FD_SET(hClntSock, &reads);
 					}
-					else
-					{
-						addrlen = sizeof(clientaddr);
-						getpeername(reads.fd_array[i], (SOCKADDR*)&clientaddr, &addrlen);
+					else {
+						// data 수신.
+						strLen = recv(reads.fd_array[i], buf, BUF_SIZE - 1, 0);
+						if (strLen <= 0)    // close request!
+						{
+							closesocket(reads.fd_array[i]);
+							FD_CLR(reads.fd_array[i], &reads);
+							printf("server> close connection with (port:%d, IP:%s)\n",
+								clientaddr.sin_port, inet_ntoa(clientaddr.sin_addr));
+							break;
+						}
+						else
+						{
+							addrlen = sizeof(clientaddr);
+							getpeername(reads.fd_array[i], (SOCKADDR*)&clientaddr, &addrlen);
 
-						buf[strLen] = '\0';
-						printf("server> (port:%d, IP:%s),Msg : %s \n",
-							clientaddr.sin_port, inet_ntoa(clientaddr.sin_addr), buf);
-						send(reads.fd_array[i], buf, strLen, 0);    // echo!
+							buf[strLen] = '\0';
+							printf("server> (port:%d, IP:%s), Msg : %s \n",
+								clientaddr.sin_port, inet_ntoa(clientaddr.sin_addr), buf);
+							send(reads.fd_array[i], buf, strLen, 0);    // echo!
+							
+							droneList[i].x = *(int*)(buf + (1 + 0 * sizeof(int)));
+							droneList[i].y = *(int*)(buf + (1 + 1 * sizeof(int)));
+							droneList[i].port = clientaddr.sin_port;
+
+							printf("드론 %d");
+							printf("cnt : %d\n", (int)buf[0]);
+							printf("command : %c\n", buf[2 * sizeof(int) + 1]);
+
+							
+						}
 					}
 				}
 			}
 		}
+	
+		// 드론 좌표 수신 후 사용자 키보드 입력을 받아 드론 이동명령 모드로 전환
+		// M을 누르는 경우 드론 이동 모드
+		// Q를 누르는 경우 드론 1차 정렬 -> 2차 이동 -> 종료 과정 수행
+
+		if (GetAsyncKeyState(0x4D)) {	// 입력 키가 M인 경우
+
+			continue;
+		}
+		if (GetAsyncKeyState(0x51)) {	// 입력 키가 Q인 경우
+			break;
+		}
 	}
+	
 	closesocket(hServSock);
 	WSACleanup();
 	return 0;
@@ -104,3 +135,13 @@ void ErrorHandling(char *message)
 	fputc('\n', stderr);
 	exit(1);
 }
+
+POS* RoutePlanner(POS* arr) {
+	for (int i = 0; i < DRONE_AMOUNT; i++) {
+		if (arr + i == NULL) {
+			ErrorHandling("[ERROR] Out of Bounds\n");
+		}
+
+	}
+}
+
